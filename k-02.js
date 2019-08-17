13658f064f73527e834fab018841bd5c3aa937a5
Heeseok <heeseok.chong@gmail.com>
2019-08-17 17:56:43 +0700

    fixed from 20103 to 20110.

--- k-02.js
+++ k-02.js
@@ -3,101 +3,138 @@ let students = [{name:'Albert'}, {name:'Bob'}, {name:'Claudia'}]
 let mix = [5, 'David', {name:'Ella'}, 23, 'Frank']
 
 // .forEach()
+// return value: undefined
 // 20101. Create a function that logs each number individually
-let logElement = (array) => array.forEach(element => console.log(element))
-logElement(numbers)
+console.log('20101. ')
+let logElements = (array) => array.forEach((element) => console.log(element))
+logElements(numbers)
 
 // 20102. Create a function that logs each student's name
-let logNames = (array) => array.forEach(element => console.log(element.name))
+console.log('20102. ')
+let logNames = (array) => array.forEach((element) => console.log(element.name))
 logNames(students)
 
 
 // .map()
+// return value: a new array
 // 20103. Create a function that adds 1 to each number and returns a new array
 // [2, 91, 79, 4, 23, 188, 22]
-let newNumbers = []
-let addOne = (array) => array.map((element) => newNumbers.push(++element))
-addOne(numbers)
-newNumbers.forEach((element) => console.log(element))
+console.log('20103. ')
+let plusOne = (element) => ++element
+let makeNewArray = (array, func) => array.map(func)
+let newNumbers = makeNewArray(numbers, plusOne)
+console.log(newNumbers)
 
 // 20104. Create a function that returns an array of  all the students' names
 // ['Albert', 'Bob', 'Claudia']
-let studentName = []
-let getName = (array) => array.forEach((element) => studentName.push(element.name))
-getName(students)
-console.log(studentName)
+console.log('20104. ')
+let getName = (element) => element.name
+let studentNames = makeNewArray(students, getName)
+console.log(studentNames)
 
 
 // .filter()
+// return value: a new array
 // 20105. Create a function that returns an array with only numbers equal to or greater than 22
 // [91, 79, 23, 188, 22]
-let filterGreater22 = (array) => array.filter(element => element >= 22)
-console.log(filterGreater22(newNumbers))
+console.log('20105. ')
+let isGreaterThanGivenNumberOrEqual = (element, givenNumber) => {
+  if (element >= givenNumber) {
+    return true
+  } else {
+    return false
+  }
+}
+
+let makeNewFilteredArray = (array, filter, func) => array.filter((element) => func(element, filter))
+let numbersGreaterThan22 = makeNewFilteredArray(newNumbers, 22, isGreaterThanGivenNumberOrEqual)
+console.log(numbersGreaterThan22)
 
 // 20106. Create a function that returns an array with only numbers greater than 
 // the parameter passed onto the function
 // 70 => [90, 78, 187]
-const greaterParameter = (array, value) => array.filter(n => n > value)
-console.log(greaterParameter(numbers, 70))
+console.log('20106. ')
+let isGreaterThanGivenNumber = (element, givenNumber) => {
+  if (element > givenNumber) {
+    return true
+  } else {
+    return false
+  }
+}
+
+let numbersGreaterThanGivenNumber = makeNewFilteredArray(numbers, 70, isGreaterThanGivenNumber)
+console.log(numbersGreaterThanGivenNumber)
 
 // 20107. Create a function that returns an array with only students 
 // whose name is more than 6 characters long
 // [{name:'Claudia'}]
-let nameGreater6 = (array) => array.filter(element => {
-  if (element.name.length > 6) {
-    console.log(element)
+console.log('20107. ')
+let isNameLengthGreaterThanGivenNumber = (element, givenNumber) => {
+  if (isGreaterThanGivenNumber(element.name.length, givenNumber)) {
+    return true
+  } else {
+    return false
   }
-})
-nameGreater6(students)
+}
+
+let nameLengthGreaterThan6 = makeNewFilteredArray(students, 6, isNameLengthGreaterThanGivenNumber)
+console.log(nameLengthGreaterThan6)
 
 // 20108. Create a function that returns an array with only strings from mix
 // ['David', 'Frank']
-let getStringOnly = (array) => array.filter(element => typeof(element) === 'string')
-console.log(getStringOnly(mix))
+console.log('20108. ')
+let getStringOnly = (array) => array.filter((element) => typeof(element) === 'string')
+let stringOnly = getStringOnly(mix)
+console.log(stringOnly)
 
 
 // .find()
+// return value: first element
 // 20109. Create a function that returns the first number greater than 100
 // 187
-let getGreater100 = (array) => array.find((element) => element > 100)
-console.log(getGreater100(numbers))
+console.log('20109. ')
+let find1stNumberGreaterThanGivenNumber = (array, givenNumber) => array.find((element) => element > givenNumber)
+console.log(find1stNumberGreaterThanGivenNumber(numbers, 100))
 
 // 20110. Create a function that returns the first student whose name is Bob
 // {
 // 	name: 'Bob'
 // }
-let getFirstNameInArray = (array, name) => array.find((element) => element.name === name)
-console.log(getFirstNameInArray(students, 'Bob'))
+console.log('20110. ')
+let findNameInArray = (array, name) => array.find((element) => element.name === name)
+console.log(findNameInArray(students, 'Bob'))
 
 // .reduce()
 // 20111. Create a function that returns the sum of all numbers using reduce()
 // 402
-let getSumOfAllNumbers = (array) => array.reduce((sum, element) => sum + element)
+console.log('20111. ')
+let getSumOfAllNumbers = (array) => array.reduce((accumulator, element) => accumulator + element)
 console.log(getSumOfAllNumbers(numbers))
 
 // 20112. Create a function that returns the sum of all numbers without using reduce()
 // 402
-let forInSum = 0
-for (index in numbers) {
-    forInSum += numbers[index]
+console.log('20112. ')
+var sum = 0
+let getSumOfElements = (array) => {
+  array.forEach((element) => sum += element)
+  return sum
 }
-console.log(forInSum)
-
-let forEachSum = 0
-numbers.forEach(element => forEachSum += element)
-console.log(forEachSum)
+console.log(sum)
 
 // 20113. Create a function that returns the difference of all numbers, using reduce()
 //  -400
+console.log('20113. ')
 console.log(numbers.reduce((t,i) => t - i))
 
 // 20114. Create a function that returns the product of all numbers
 // 1819457640
+console.log('20114. ')
 console.log(numbers.reduce((t, i) => t * i))
 
 // .sort()
 // 20115. Create a function that returns an array with numbers sorted in descending order
 // [187, 90, 78, 22, 21, 3, 1]
+console.log('20115. ')
 let compareNumbers = (a, b) => a - b
 let sortedNumbers = numbers.sort(compareNumbers)
 console.log(sortedNumbers.reverse())
@@ -105,7 +142,7 @@ console.log(sortedNumbers.reverse())
 // 20116. Create a function that returns an array with students sorted by ascending 
 // length of their names
 // [{name:'Bob'},{name:'Albert'},{name:'Claudia'}]
-
+console.log('20116. ')
 
 // Math
 let products = [{name:'bread',price:3.5},{name:'orange',price:1.3},{name:'gum',price:2.8}]
@@ -115,11 +152,13 @@ let products = [{name:'bread',price:3.5},{name:'orange',price:1.3},{name:'gum',p
 // removing any decimals
 // 	3.14 => 3
 // 	15.9 => 16
+console.log('20201. ')
 let getRoundNumber = (number) => Math.round(number)
 let logRoundNumber = (array) => array.forEach((element) => Math.round(element))
 
 // 20202. Create a function that returns all products with their prices rounded
 // [{name:'bread',price:4},{name:'orange',price:1},{name:'gum',price:3}]
+console.log('20202. ')
 let productAfterRounding = (array) => {
   array.forEach((element) => element.price = getRoundNumber(element.price))
   return array
@@ -128,6 +167,7 @@ console.log(productAfterRounding(products))
 
 // 20203. Create a function that takes 2 numbers as parameters and returns 
 // the first number rounded to the number of decimals defined by the second parameter.
+console.log('20203. ')
 let round2precision = (number, precision) => {
   remainder = number % precision
   if ((remainder/precision) >= 0.5) {
@@ -143,6 +183,7 @@ console.log(round2precision(10, 7))
 // Power
 // 20204. Create a function that that takes 2 numbers as parameters and returns 
 // the first number to the power of the second number.
+console.log('20204. ')
 let getPower = (number, exponentialNumber) => {
   if (exponentialNumber == 0) {
     return 1
@@ -154,10 +195,12 @@ console.log(getPower(2,9))
 
 // Random
 // 20205. Create a function that returns a random number between 1 and 10.
+console.log('20205. ')
 let getRandomNumber = () => Math.floor(Math.random() * 10 + 1)
 console.log(getRandomNumber())
 
 // 20206. Create a function that takes a number as parameter and returns 
 // a random number between 1 and the parameter.
+console.log('20206. ')
 let getRandomNumberFromOneToGivenNumber = (givenNumber) => Math.floor(Math.random() * givenNumber + 1)
 console.log(getRandomNumber(20))
