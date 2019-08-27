517c8fc87ca102ae8dbdbd3057bfa5672427afbf
Heeseok <heeseok.chong@gmail.com>
2019-08-27 10:54:37 +0700

    not declared total variable in #050502.

--- k-05-dom/#05050X-Forms/#050502-Forms/index.js
+++ k-05-dom/#05050X-Forms/#050502-Forms/index.js
@@ -1,17 +1,14 @@
 // #050502
-//     Given the below html, change the content of the element with an id of total to the sum of
-//     the values typed inside the 2 input fields.
-//     Make sure the total is displayed in real-time as the values are typed in.
+// Given the below html, change the content of the element with an id of total to the sum of
+// the values typed inside the 2 input fields.
+// Make sure the total is displayed in real-time as the values are typed in.
 
 // <input type="number" class="number" placeholder="Number 1">
 // <input type="number" class="number" placeholder="Number 2">
 // <label>Total: <span id="total"></span></label>
 
 let refreshTotal = () => {
-  let total = 0;
-  Array.from(document.getElementsByClassName('number')).forEach(
-    e => (total += Number(e.value))
-  );
-
-  document.getElementById('total').innerHTML = total;
+  document.getElementById('total').innerHTML = Array.from(
+    document.getElementsByClassName('number')
+  ).reduce((a, b) => a + b);
 };
