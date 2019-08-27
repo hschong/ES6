47d942edb2b5d735fd9df46a50d8e6b444a1c097
Heeseok <heeseok.chong@gmail.com>
2019-08-27 10:24:50 +0700

    fixed #050403

--- k-05-dom/#05040X-Classes/#050403-Classes/index.js
+++ k-05-dom/#05040X-Classes/#050403-Classes/index.js
@@ -8,9 +8,14 @@
 //       <li class="selected">3</li>
 //     </ul>
 
-arraySelected = Array.from(document.getElementsByClassName('selected'));
-arraySelected.forEach(e => {
-  if (e.classList.contains('primary') === false) {
-    e.classList.remove('selected');
-  }
-});
+let removeClassInElement = givenClass => {
+  Array.from(document.getElementsByClassName(givenClass)).forEach(e => {
+    if (e.classList.contains('primary') === false) {
+      e.classList.remove(givenClass);
+    }
+  });
+};
+
+// Get all the elements with a class selected
+// loop through these elements
+// if ! primary, remove selected
