70bd043c4ada7a3d9f12dd1e85ad5f929da82e14
Heeseok <heeseok.chong@gmail.com>
2019-09-01 02:34:27 +0700

    made them prettier

--- k-02-js/k-02-01-loops.js
+++ k-02-js/k-02-01-loops.js
@@ -1,182 +1,180 @@
-let numbers = [1, 90, 78, 3, 22, 187, 21]
-let students = [{name:'Albert'}, {name:'Bob'}, {name:'Claudia'}]
-let mix = [5, 'David', {name:'Ella'}, 23, 'Frank']
-
+let numbers = [1, 90, 78, 3, 22, 187, 21];
+let students = [{ name: 'Albert' }, { name: 'Bob' }, { name: 'Claudia' }];
+let mix = [5, 'David', { name: 'Ella' }, 23, 'Frank'];
 
 // .forEach() => return value: undefined
 // 020101. Create a function that logs each number individually
-console.log('20101. ')
-let logElements = (array) => array.forEach((element) => console.log(element))
-logElements(numbers)
-
+console.log('20101. ');
+let logElements = array => array.forEach(e => console.log(e));
+logElements(numbers);
 
 // 020102. Create a function that logs each student's name
-console.log('20102. ')
-let logNames = (array) => array.forEach((element) => console.log(element.name))
-logNames(students)
-
+console.log('20102. ');
+let logNames = array => array.forEach(e => console.log(e.name));
+logNames(students);
 
 // .map() => return value: a new array
 // 020103. Create a function that adds 1 to each number and returns a new array
 // [2, 91, 79, 4, 23, 188, 22]
-let plusOne = (element) => ++element
-let makeNewArray = (array, func) => array.map(func)
-let newNumbers = makeNewArray(numbers, plusOne)
+let plusOne = e => ++e;
+let makeNewArray = (array, func) => array.map(func);
+let newNumbers = makeNewArray(numbers, plusOne);
 // console.log('#20103 ===> ', newNumbers)
-console.log('#20103 ===> ', numbers.map((element)=>element+1))
-
+console.log('#20103 ===> ', numbers.map(e => e + 1));
 
 // 020104. Create a function that returns an array of  all the students' names
 // ['Albert', 'Bob', 'Claudia']
-console.log('20104. ')
-let getName = (element) => element.name
-let studentNames = makeNewArray(students, getName)
-console.log(studentNames)
-
+console.log('20104. ');
+let getName = e => e.name;
+let studentNames = makeNewArray(students, getName);
+console.log(studentNames);
 
 // .filter() => return value: a new array
-// 020105. Create a function that returns an array with only numbers equal to 
+// 020105. Create a function that returns an array with only numbers equal to
 // or greater than 22
 // [91, 79, 23, 188, 22]
-console.log('20105. ')
-let isGreaterThanGivenNumberOrEqual = (element, givenNumber) => {
-  if (element >= givenNumber) {
-    return true
+console.log('20105. ');
+let isGreaterThanGivenNumberOrEqual = (e, givenNumber) => {
+  if (e >= givenNumber) {
+    return true;
   } else {
-    return false
+    return false;
   }
-}
+};
 
-let makeNewFilteredArray = (array, filter, func) => array.filter((element) => func(element, filter))
-let numbersGreaterThan22 = makeNewFilteredArray(newNumbers, 22, isGreaterThanGivenNumberOrEqual)
-console.log(numbersGreaterThan22)
-
+let makeNewFilteredArray = (array, filter, func) =>
+  array.filter(e => func(e, filter));
+let numbersGreaterThan22 = makeNewFilteredArray(
+  newNumbers,
+  22,
+  isGreaterThanGivenNumberOrEqual
+);
+console.log(numbersGreaterThan22);
 
 // 020106. Create a function that returns an array with only numbers greater
 // than the parameter passed onto the function
 // 70 => [90, 78, 187]
-console.log('20106. ')
-let isGreaterThanGivenNumber = (element, givenNumber) => {
-  if (element > givenNumber) {
-    return true
+console.log('20106. ');
+let isGreaterThanGivenNumber = (e, givenNumber) => {
+  if (e > givenNumber) {
+    return true;
   } else {
-    return false
+    return false;
   }
-}
+};
 
-let numbersGreaterThanGivenNumber = makeNewFilteredArray(numbers, 70, isGreaterThanGivenNumber)
-console.log(numbersGreaterThanGivenNumber)
+let numbersGreaterThanGivenNumber = makeNewFilteredArray(
+  numbers,
+  70,
+  isGreaterThanGivenNumber
+);
+console.log(numbersGreaterThanGivenNumber);
 
-
-// 020107. Create a function that returns an array with only students 
+// 020107. Create a function that returns an array with only students
 // whose name is more than 6 characters long
 // [{name:'Claudia'}]
-console.log('20107. ')
-let isNameLengthGreaterThanGivenNumber = (element, givenNumber) => {
-  if (isGreaterThanGivenNumber(element.name.length, givenNumber)) {
-    return true
+console.log('20107. ');
+let isNameLengthGreaterThanGivenNumber = (e, givenNumber) => {
+  if (isGreaterThanGivenNumber(e.name.length, givenNumber)) {
+    return true;
   } else {
-    return false
+    return false;
   }
-}
+};
 
-let nameLengthGreaterThan6 = makeNewFilteredArray(students, 6, isNameLengthGreaterThanGivenNumber)
-console.log(nameLengthGreaterThan6)
-
+let nameLengthGreaterThan6 = makeNewFilteredArray(
+  students,
+  6,
+  isNameLengthGreaterThanGivenNumber
+);
+console.log(nameLengthGreaterThan6);
 
 // 020108. Create a function that returns an array with only strings from mix
 // ['David', 'Frank']
-console.log('20108. ')
-let getStringOnly = (array) => array.filter((element) => typeof(element) === 'string')
-let stringOnly = getStringOnly(mix)
-console.log(stringOnly)
-
+console.log('20108. ');
+let getStringOnly = array => array.filter(e => typeof e === 'string');
+let stringOnly = getStringOnly(mix);
+console.log(stringOnly);
 
 // .find() => return value: first element
 // 020109. Create a function that returns the first number greater than 100
 // 187
-console.log('20109. ')
-let find1stNumberGreaterThanGivenNumber = (array, givenNumber) => array.find((element) => element > givenNumber)
-console.log(find1stNumberGreaterThanGivenNumber(numbers, 100))
-
+console.log('20109. ');
+let find1stNumberGreaterThanGivenNumber = (array, givenNumber) =>
+  array.find(e => e > givenNumber);
+console.log(find1stNumberGreaterThanGivenNumber(numbers, 100));
 
 // 020110. Create a function that returns the first student whose name is Bob
 // {
 // 	name: 'Bob'
 // }
-console.log('20110. ')
-let findNameInArray = (array, name) => array.find((element) => element.name === name)
-console.log(findNameInArray(students, 'Bob'))
-
+console.log('20110. ');
+let findNameInArray = (array, name) => array.find(e => e.name === name);
+console.log(findNameInArray(students, 'Bob'));
 
 // .reduce() => return value: tthe single value that results from the reduction.
 // 020111. Create a function that returns the sum of all numbers using reduce()
 // 402
-console.log('20111. ')
-let getSumOfAllNumbers = (array) => array.reduce((accumulator, element) => accumulator + element)
-console.log(getSumOfAllNumbers(numbers))
-
+console.log('20111. ');
+let getSumOfAllNumbers = array => array.reduce((acc, cur) => acc + cur);
+console.log(getSumOfAllNumbers(numbers));
 
 // 020112. Create a function that returns the sum of all numbers without using reduce()
 // 402
-console.log('20112. ')
-let getSumOfAllElements = (array) => {
-  let result = 0
-  array.forEach((element) => result += element)
-  return result
-}
-console.log(getSumOfAllElements(numbers))
-
+console.log('20112. ');
+let getSumOfAllElements = array => {
+  let result = 0;
+  array.forEach(e => (result += e));
+  return result;
+};
+console.log(getSumOfAllElements(numbers));
 
 // 020113. Create a function that returns the difference of all numbers, using reduce()
 //  -400
-console.log('20113. ')
-let getDifferenceOfAllElements = (array) => array.reduce((prevValue, curValue) => prevValue - curValue) 
-console.log(getDifferenceOfAllElements(numbers))
-
+console.log('20113. ');
+let getDifferenceOfAllElements = array =>
+  array.reduce((prevValue, curValue) => prevValue - curValue);
+console.log(getDifferenceOfAllElements(numbers));
 
 // 020114. Create a function that returns the product of all numbers
 // 1819457640
-console.log('20114. ')
-let getProductOfAllElements = (array) => array.reduce((prevValue, curValue) => prevValue *= curValue)
-console.log(getProductOfAllElements(numbers))
-
+console.log('20114. ');
+let getProductOfAllElements = array =>
+  array.reduce((prevValue, curValue) => (prevValue *= curValue));
+console.log(getProductOfAllElements(numbers));
 
 // .sort() ==> return value: The sorted array.
 // 020115. Create a function that returns an array with numbers sorted in descending order
 // [187, 90, 78, 22, 21, 3, 1]
-console.log('20115. ')
-let Ascending = Symbol('Ascending')
-let Descending = Symbol('Descending')
-let compareNumbers = (a, b) => a - b
+console.log('20115. ');
+let Ascending = Symbol('Ascending');
+let Descending = Symbol('Descending');
+let compareNumbers = (a, b) => a - b;
 
 let sorting = (array, direction) => {
   switch (direction) {
     case Ascending:
-      array.sort(compareNumbers)
-      break
+      array.sort(compareNumbers);
+      break;
 
     case Descending:
-      array.sort(compareNumbers)
-      array.reverse()
-      break
-    
+      array.sort(compareNumbers);
+      array.reverse();
+      break;
+
     default:
-      console.log('ordering direction is not defined.')
+      console.log('ordering direction is not defined.');
   }
 
-  return array
-}
-console.log(sorting(numbers, Descending))
+  return array;
+};
+console.log(sorting(numbers, Descending));
 
-
-// 020116. Create a function that returns an array with students sorted 
-// by ascending 
+// 020116. Create a function that returns an array with students sorted
+// by ascending
 // [{name:'Bob'},{name:'Albert'},{name:'Claudia'}]
 
-console.log('20116. ')
-
-
+console.log('20116. ');
 
 // Various
 // #020117
@@ -194,12 +192,10 @@ console.log('20116. ')
 //   }
 // ]
 
-
 // #020118
-// In the following array, number 6 is missing. Create a function that returns 
+// In the following array, number 6 is missing. Create a function that returns
 // the missing number.
 // [1, 2, 3, 4, 5, 7, 8, 9, 10]
-
 
 // #020119
 // Given the following currency denominations
@@ -222,8 +218,6 @@ console.log('20116. ')
 // }
 // console.log(denominationFunction(dens,27.58))
 
-
-
-// create a function that returns an array with separate denominations needed to 
+// create a function that returns an array with separate denominations needed to
 // reach the amount passed into the function. Optimize for higher denominations.
 // 27.58 => [10, 10, 5, 1, 1, 0.5, 0.05, 0.01, 0.01, 0.01]
--- k-02-js/k-02-05-class.js
+++ k-02-js/k-02-05-class.js
@@ -1,141 +1,145 @@
 // Classes
 // #020501
 class Car {
-    constructor (make, model, color) {
-        this.make = make
-        this.model = model
-        this.color = color
-    }
+  constructor(make, model, color) {
+    this.make = make;
+    this.model = model;
+    this.color = color;
+  }
 }
 
-let buildCar = () => new Car('Benz', 'E300', 'White')
-console.log('#020501', buildCar())
+let buildCar = () => new Car('Benz', 'E300', 'White');
+console.log('#020501', buildCar());
 
-  
 // #020502
-let names = ['Alice', 'Bernard', 'Catherine', 'David']
-let jobs = ['Accountant', 'Biz Manager', 'Cleaner', 'Director']
+let names = ['Alice', 'Bernard', 'Catherine', 'David'];
+let jobs = ['Accountant', 'Biz Manager', 'Cleaner', 'Director'];
 
 class Employee {
-    constructor(name, job) {
-        this.name = name
-        this.job = job
-    }
+  constructor(name, job) {
+    this.name = name;
+    this.job = job;
+  }
 }
 
-let makeEmployeeInstance = (index) => employee = new Employee(names[index], jobs[index])
-let makeEmployeeArray = (names, jobs) => names.map(
-    (name, index) => makeEmployeeInstance(index))
+let makeEmployeeInstance = index =>
+  (employee = new Employee(names[index], jobs[index]));
+let makeEmployeeArray = (names, jobs) =>
+  names.map((name, index) => makeEmployeeInstance(index));
 
-console.log('#020502', makeEmployeeArray(names, jobs))
-
+console.log('#020502', makeEmployeeArray(names, jobs));
 
 // #020503
-let belts = ['white', 'yellow', 'orange', 'green', 'blue', 'brown', 'black']
+let belts = ['white', 'yellow', 'orange', 'green', 'blue', 'brown', 'black'];
 class Karateka {
-    constructor(name) {
-        this.name = name
-        this.belt = belts[0]
-    }
+  constructor(name) {
+    this.name = name;
+    this.belt = belts[0];
+  }
 
-    getBelt() {
-        return this.belt
-    }
+  getBelt() {
+    return this.belt;
+  }
 
-    setBelt(index) {
-        this.belt = belts[index]
-    }
+  setBelt(index) {
+    this.belt = belts[index];
+  }
 
-    hasBlackBelt() {
-        return this.getBelt() === 'black' ? true : false
-    }
+  hasBlackBelt() {
+    return this.getBelt() === 'black' ? true : false;
+  }
 
-    getIndexOfBelt() {
-        return belts.indexOf(this.getBelt())
-    }
-    
-    judge(score) {
-        if (score > UPGRADE_SCORE) {
-            if (this.hasBlackBelt() === false) {
-                this.increaseRank(this.getIndexOfBelt())
-            }
-        }
-    }
+  getIndexOfBelt() {
+    return belts.indexOf(this.getBelt());
+  }
 
-    increaseRank(index) {
-        this.setBelt(++index)
+  judge(score) {
+    if (score > UPGRADE_SCORE) {
+      if (this.hasBlackBelt() === false) {
+        this.increaseRank(this.getIndexOfBelt());
+      }
     }
+  }
+
+  increaseRank(index) {
+    this.setBelt(++index);
+  }
 }
 
-const UPGRADE_SCORE = 8
-const MAX_SCORE = 10
-const INTERVAL_SECONDS = 1000
-const MAX_FIGHT_SECONDS = 20000
+const UPGRADE_SCORE = 8;
+const MAX_SCORE = 10;
+const INTERVAL_SECONDS = 1000;
+const MAX_FIGHT_SECONDS = 20000;
 
-let getRandomNumber = max => Math.floor(Math.random() * max) + 1
-let getScore = () => getRandomNumber(MAX_SCORE)
+let getRandomNumber = max => Math.floor(Math.random() * max) + 1;
+let getScore = () => getRandomNumber(MAX_SCORE);
 
 let logResult = () => {
-    let score = getScore()
-    fighter.judge(score)
-    console.log('Score: ', score)
-    console.log(fighter)
-} 
+  let score = getScore();
+  fighter.judge(score);
+  console.log('Score: ', score);
+  console.log(fighter);
+};
 
-let fight = (ins) => {
-    let intervalID = setInterval(logResult, INTERVAL_SECONDS)
+let fight = ins => {
+  let intervalID = setInterval(logResult, INTERVAL_SECONDS);
 
-    setTimeout(() => {
-        clearInterval(intervalID)
-    }, MAX_FIGHT_SECONDS)
-}
+  setTimeout(() => {
+    clearInterval(intervalID);
+  }, MAX_FIGHT_SECONDS);
+};
 
-let fighter = new Karateka('Heeseok')
-console.log('#020503', fighter)
-fight(fighter)
-
+let fighter = new Karateka('Heeseok');
+console.log('#020503', fighter);
+fight(fighter);
 
 // #020504
-let fruitNames = ['apples', 'bananas', 'bread', 'cookies', 'broccoli', 'onions']
-let prices = [20, 12, 24, 53, 32, 15]
-let discounts = [0, 0, 10, 25, 0, 5]
+let fruitNames = [
+  'apples',
+  'bananas',
+  'bread',
+  'cookies',
+  'broccoli',
+  'onions'
+];
+let prices = [20, 12, 24, 53, 32, 15];
+let discounts = [0, 0, 10, 25, 0, 5];
 
 class Product {
-    constructor(name, price) {
-        this.name = name
-        this.price = price
-    }
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+  }
 
-    applyDiscount(discount) {
-        this.price = this.price - (this.price * discount / 100)
-    }
+  applyDiscount(discount) {
+    this.price = this.price - (this.price * discount) / 100;
+  }
 }
 
 class Receipt {
-    constructor(products) {
-        this.products = products
-    }
+  constructor(products) {
+    this.products = products;
+  }
 
-    calcTotal() {
-        let total = 0
-        this.products.forEach(product => total += product.price)
-        return total
-    }
+  calcTotal() {
+    return this.products.reduce((acc, cur) => acc + cur);
+  }
 }
 
-let makeNewProductsAfterDiscounting = () => fruitNames.map((name, index) => {
-    product = new Product(name, prices[index])
-    product.applyDiscount(discounts[index])
-    return product
-})
+let makeNewProductsAfterDiscounting = () =>
+  fruitNames.map((name, index) => {
+    product = new Product(name, prices[index]);
+    product.applyDiscount(discounts[index]);
+    return product;
+  });
 
-let logReceipt = ()=> {
-    console.log('#020504')
-    console.log('Receipt: [')
-    receipt = new Receipt(makeNewProductsAfterDiscounting())
-    receipt.products.forEach(product => console.log(product))
-    console.log(']')
-    console.log(`Total: ${receipt.calcTotal()}`)
-}
+let logReceipt = () => {
+  console.log('#020504');
+  console.log('Receipt: [');
+  receipt = new Receipt(makeNewProductsAfterDiscounting());
+  receipt.products.forEach(product => console.log(product));
+  console.log(']');
+  console.log(`Total: ${receipt.calcTotal()}`);
+};
 
-logReceipt()
+logReceipt();
