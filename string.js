3dcfde41a126168fa10acc480099462e971ee971
HEESEOK <heeseok.chong@gmail.com>
2020-10-11 05:40:21 +0900

    initial commit for string manipulation

---
+++ string.js
@@ -0,0 +1,11 @@
+// String Manipulation
+
+
+// iterate through string 
+const name = 'heeseok';
+
+// With ES6
+[...name].forEach(e => console.log(e)) 
+
+// with ES5:
+name.split('').forEach(e => console.log(e));
